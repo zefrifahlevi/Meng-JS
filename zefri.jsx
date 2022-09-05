87003a98dcce45341e2b64f3d2869890bd5035ec
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2022-09-05 19:21:44 +0700

    Updating Data

--- zefri.jsx
+++ zefri.jsx
@@ -1,10 +1,32 @@
-const nama = ['zefri', 'drian', 'patrzalek']
+const nama = [
+    'zefri',
+    'drian',
+    'patrzalek',
+    'driansyah',
+] 
 
 nama.splice();
-console.log(nama);
+console.log(nama); 
 
-console.log(nama[0])
+console.log(nama[0]);
 
 for (const i of nama) {
     console.log(i)
+} 
+
+let akun_zefri = 10
+let bestie = 4
+
+function akun() {
+    for(let i = 0; i <= akun_zefri; i++) {
+        if(i = 0) {
+            console.log('')
+        }
+        else if (i % 2 == 0) {
+            console.log(i,'Merupakan BIlangan genap')
+        }
+        else {
+            console.log(i,'Merupakan bilangan ganjil')
+        }
+    }
 }
